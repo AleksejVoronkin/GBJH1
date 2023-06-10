1f04b120e1d8300d573388b288ca4e0d74b14f93
AleksejVoronkin <120055748+AleksejVoronkin@users.noreply.github.com>
2023-06-10 22:38:24 +0300

    Add files via upload

---
+++ J3.java
@@ -0,0 +1,43 @@
+
+// 3) Реализовать простой калькулятор
+// + - / *
+
+// Введите первое число: A \n
+// Введите знак: *
+// Введите второе число: B
+// Результат: A*B
+
+// sc.next() -- считывает символ
+// sc.nextLine() -- считывает всю строчку
+// sc.nextInt() -- считывает число, но без перехода на след. строчку
+package JAVA;
+import java.util.Scanner;
+public class J3 {
+    public static void main (String[] args){ 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Введите первое число: ");
+    int a = sc.nextInt();
+    sc.nextLine();
+
+    System.out.println("Введите знак(+, -, /, *):");
+    String c = sc.nextLine();
+    
+    System.out.println("Введите Второе число: ");
+    int b = sc.nextInt();
+    int res = 0;
+    if (c.equals("+")){
+        res = a + b;
+        }
+    else if (c.equals("-")){
+        res = a - b;
+        }
+    else if (c.equals("/")){
+        res = a / b;
+        }
+    else if (c.equals("*")){
+        res = a * b;
+        }
+    System.out.println(res);
+    }
+}
+
